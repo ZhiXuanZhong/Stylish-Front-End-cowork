5069e76d6a0f9c9b88862c8076fc437b8b911d94
Eric Tsai <eric492718@gmail.com>
2023-06-06 12:10:15 +0800

    completed the message style in chatbot.

--- src/components/Chatbot/Message.js
+++ src/components/Chatbot/Message.js
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 import chatbotIcon from './img/chatbot-icon.png';
+import {v4 as uuidv4} from 'uuid';
 
 const MessageWrapper = styled.div`
   margin: 15px 20px;
@@ -9,12 +10,13 @@ const MessageWrapper = styled.div`
 `;
 
 const ChatbotAvatar = styled.img`
-  width: 30px;
+  width: 40px;
 `;
 
 const ContentBoxWrapper = styled.div`
   max-width: 80%;
   background-color: ${props => props.backgroundColor};
+  color: ${props => props.color};
   border-radius: 24px;
   margin-top: ${props => props.marginTop};
 `;
@@ -26,21 +28,44 @@ const Content = styled.div`
 
 export function Message({messages}) {
   return (
-    <MessageWrapper alignItems={'flex-end'}>
-      <ChatbotAvatar src={chatbotIcon} />
-      {messages.map((message, idx) => {
-        return (
-          <ContentBoxWrapper
-            key={idx}
-            backgroundColor={'#ffffff'}
-            marginTop={idx === 0 ? '0px' : '12px'}
-            onClick={() => {
-              console.log(idx);
-            }}>
-            <Content key={idx}>{message}</Content>
-          </ContentBoxWrapper>
-        );
+    <>
+      {messages.texts.map((textArray, idx) => {
+        return textArray.map(text => {
+          return (
+            <MessageWrapper
+              key={uuidv4()}
+              alignItems={messages.style[idx].alignItems}>
+              <ChatbotAvatar src={messages.style[idx].avatar} />
+              <ContentBoxWrapper
+                backgroundColor={messages.style[idx].backgroundColor}
+                color={messages.style[idx].color}
+                marginTop={idx === 0 ? '0px' : '12px'}
+                onClick={() => {
+                  console.log(idx);
+                }}>
+                <Content>{text}</Content>
+              </ContentBoxWrapper>
+            </MessageWrapper>
+          );
+        });
       })}
-    </MessageWrapper>
+    </>
+
+    // <MessageWrapper alignItems={'flex-end'}>
+    //   <ChatbotAvatar src={chatbotIcon} />
+    //   {messages.texts.map((text, idx) => {
+    //     return (
+    //       <ContentBoxWrapper
+    //         key={idx}
+    //         backgroundColor={messages.style[idx].backgroundColor}
+    //         marginTop={idx === 0 ? '0px' : '12px'}
+    //         onClick={() => {
+    //           console.log(idx);
+    //         }}>
+    //         <Content key={idx}>{text}</Content>
+    //       </ContentBoxWrapper>
+    //     );
+    //   })}
+    // </MessageWrapper>
   );
 }
