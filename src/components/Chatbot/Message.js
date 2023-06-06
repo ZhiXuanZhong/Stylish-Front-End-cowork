ffd4f5713510c9cd5a3c2dd2f7d401a833905124
Eric Tsai <eric492718@gmail.com>
2023-06-06 09:40:07 +0800

    Add scrolling feature on the chatbot

--- src/components/Chatbot/Message.js
+++ src/components/Chatbot/Message.js
@@ -2,8 +2,10 @@ import styled from 'styled-components';
 import chatbotIcon from './img/chatbot-icon.png';
 
 const MessageWrapper = styled.div`
-  margin-left: 20px;
-  margin-top: 15px;
+  margin: 15px 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: ${props => props.alignItems};
 `;
 
 const ChatbotAvatar = styled.img`
@@ -12,29 +14,30 @@ const ChatbotAvatar = styled.img`
 
 const ContentBoxWrapper = styled.div`
   max-width: 80%;
-  /* height: 60px; */
-  background-color: #ffffff;
   background-color: ${props => props.backgroundColor};
   border-radius: 24px;
+  margin-top: ${props => props.marginTop};
 `;
 
 const Content = styled.div`
   padding: 15px;
   line-height: 23px;
-  margin-top: ${props => props.marginTop};
 `;
 
 export function Message({messages}) {
   return (
-    <MessageWrapper>
+    <MessageWrapper alignItems={'flex-end'}>
       <ChatbotAvatar src={chatbotIcon} />
-
       {messages.map((message, idx) => {
         return (
-          <ContentBoxWrapper key={idx}>
-            <Content key={idx} marginTop={idx === 0 ? '0px' : '12px'}>
-              {message}
-            </Content>
+          <ContentBoxWrapper
+            key={idx}
+            backgroundColor={'#ffffff'}
+            marginTop={idx === 0 ? '0px' : '12px'}
+            onClick={() => {
+              console.log(idx);
+            }}>
+            <Content key={idx}>{message}</Content>
           </ContentBoxWrapper>
         );
       })}
